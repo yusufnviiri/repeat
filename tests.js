4c1ddeb46f62158d294fc6700368582574365e72
yusufnviiri <yusufnviiri@gmail.com>
2022-03-30 14:40:56 +0300

    Add ruke for negative numbers

--- tests.js
+++ tests.js
@@ -1,14 +1,13 @@
 function testing(oldstrng, n) {
-    let newStrng = oldstrng;
-  
-    let noNumb="' '"
-    for (i = 1; i < n; i++) {
-      newStrng = newStrng + newStrng;
-      newStrng = newStrng;
-    } 
-    if (n == 0||n<0) {
-      return noNumb;
-    } else return newStrng
-  }
-  console.log(testing("purple", 4));
+  let newStrng=oldstrng ;
+  let noNumb="' '"
+  for (i = 1; i < n; i++) {
+    newStrng = oldstrng + newStrng;
+    newStrng = newStrng;
+     } 
+  if (n == 0||n<0) {
+    return noNumb;
+  } else return newStrng
+}
+console.log(testing("maverick", -4));
   
